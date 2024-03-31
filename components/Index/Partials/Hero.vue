bd7e376016c92f960cd7ef26d978504b65d18220
MaxFelis <m.felis@flooris.nl>
2024-03-31 16:32:17 +0200

    86by43m28 Hero component
    - added responsive hero image

--- components/Index/Partials/Hero.vue
+++ components/Index/Partials/Hero.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="relative md:h-4/6 overflow-hidden -mt-10 md:mt-0">
-    <NuxtImg
-        provider="strapi"
-        :src="hero.image.data.attributes.formats.large.url"
-        alt="hero-image"
-        width='1728px'
-        height='945px'
-        fit='cover'
-        format='webp'
-        quality='100'
-    />
+    <div class="responsive-image-wrapper">
+      <NuxtImg
+          provider="strapi"
+          :src="hero.image.data.attributes.formats.large.url"
+          alt="hero-image"
+          class="nuxt-img-responsive"
+          fit='cover'
+          loading="lazy"
+          format='webp'
+          quality="auto"
+      />
+    </div>
     <div class="absolute top-0 left-0 bottom-0 w-1/2 bg-black bg-opacity-75 flex justify-center items-center flex-col">
       <div class="pl-4 md:pl-0 md:h-80">
         <h1 class="text-white text-2xl md:text-[54px] break-words md:w-96 font-didact md:leading-[55px]">
@@ -37,3 +39,25 @@ defineProps({
   }
 })
 </script>
+
+<style>
+.responsive-image-wrapper {
+  position: relative;
+  width: 100%; /* Use 100% width for responsiveness */
+  padding-top: 56.25%; /* This is an example for a 16:9 aspect ratio (height / width * 100%) */
+}
+.nuxt-img-responsive {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover; /* Ensure the image covers the container without losing aspect ratio */
+}
+@media (max-width: 768px) {
+  .nuxt-img-responsive {
+    width: 100%; /* Adjust width and height as necessary */
+    height: auto;
+  }
+}
+</style>
