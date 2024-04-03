5b36043ad7a1dd759b3809d0eee61a9d867b15de
MaxFelis <m.felis@flooris.nl>
2024-04-03 20:48:39 +0200

    86by43m3d Footer
    - updated homepage

--- components/Index/HomePage.vue
+++ components/Index/HomePage.vue
@@ -2,7 +2,6 @@
   <div class="flex flex-col lg:gap-y-32 h-full lg:mt-32">
     <div class="px-6 lg:px-24 2xl:px-32 flex flex-col lg:gap-y-32">
       <RoofConstructions :roof-construction-data="pageData.roofConstructionComponent"/>
-      <RoadMap :roadmap-component-data="pageData.Roadmap"/>
     </div>
     <ImageWithText :image-with-text-data="pageData.aboutUs"/>
     <div class="px-6 lg:px-24 2xl:px-32 flex flex-col lg:gap-y-32">
@@ -12,7 +11,6 @@
 </template>
 <script setup lang="ts">
 import RoofConstructions from "~/components/Index/Partials/RoofConstructions.vue";
-import RoadMap from "~/components/Index/Partials/RoadMap.vue";
 import ImageWithText from "~/components/General/ImageWithText.vue";
 import InformationBlocks from "~/components/General/InformationBlocks.vue";
 
