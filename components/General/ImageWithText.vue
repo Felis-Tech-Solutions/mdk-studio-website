ba321639966dfaab22ca5bee6ecfd734b96e5df0
MaxFelis <m.felis@flooris.nl>
2024-04-10 22:17:55 +0200

    86by43m1f Home page
    - deploy test

--- components/General/ImageWithText.vue
+++ components/General/ImageWithText.vue
@@ -3,7 +3,7 @@
       class="md:max-w-screen h-auto flex flex-col  px-0 md:px-24 lg:px-52 2xl:px-[18vw] md:py-32 gap-x-20 mt-8 lg:mt-0 pb-10"
       :class="imageWithTextData.imageType === 'ImageRight' ? 'lg:flex-row-reverse ' : 'lg:flex-row bg-primary text-white'"
   >
-    <div class="lg:w-1/2 px-8 lg:px-0 pt-6 lg:pt-0 2xl:mx-24">
+    <div class="lg:w-1/2 px-8 lg:px-0 pt-12 pb-6 lg:[b-0 lg:pt-0 2xl:mx-24">
       <NuxtImg
           provider="strapi"
           class="w-full 2xl:w-[45rem] h-[20rem] 2xl:h-[30rem] object-cover rounded-2xl"
@@ -15,8 +15,8 @@
       />
     </div>
     <div class="lg:w-1/2 flex items-center md:items-start flex-col mt-4 md:mt-0 px-6 md:px-0">
-      <div class="w-auto">
-        <span class="text-[32px] pr-24 font-bold">
+      <div class="w-full md:w-auto lg:flex lg:flex-col lg:items-center">
+        <span class="text-[32px] md:pr-24 font-bold text">
           {{ imageWithTextData.title }}
         </span>
         <hr class="border-b-8 w-full rounded-2xl" :class="imageWithTextData.imageType === 'ImageRight' ? 'border-primary' : 'border-white'">
