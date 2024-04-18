9dea2d7dad2bb4c8ff77519963ad880664caf70b
MaxFelis <m.felis@flooris.nl>
2024-04-18 21:26:48 +0200

    86byabm9u Seo optimisatie
    - added favicon

--- components/Index/HomePage.vue
+++ components/Index/HomePage.vue
@@ -2,7 +2,9 @@
   <div class="flex flex-col lg:gap-y-32 h-full lg:mt-24">
     <div class="px-6 lg:px-24 xl:px-40 2xl:px-52 3xl:px-72 4xl:px-80 flex flex-col lg:gap-y-32 lg:mb-24">
       <RoofConstructions :roof-construction-data="pageData.roofConstructionComponent" v-if="pageData.roofConstructionComponent" class="2xl:mx-24"/>
+      <Roadmap :roadmap-component-data="pageData.Roadmap" v-if="pageData.Roadmap"/>
     </div>
+
     <ImageWithText :image-with-text-data="pageData.aboutUs" v-if="pageData.aboutUs"/>
     <div class="px-6 lg:px-24 2xl:px-80 flex flex-col lg:gap-y-32">
       <InformationBlocks :information-blocks-data="pageData.informationBlocks" v-if="pageData.informationBlocks" class="2xl:mx-24"/>
@@ -13,6 +15,7 @@
 import RoofConstructions from "~/components/Index/Partials/RoofConstructions.vue";
 import ImageWithText from "~/components/General/ImageWithText.vue";
 import InformationBlocks from "~/components/General/InformationBlocks.vue";
+import Roadmap from "~/components/Index/Partials/Roadmap.vue";
 
 defineProps({
   pageData: {
