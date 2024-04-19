0709cc1cfee5bb3c8ecc677c286d7230da3f4941
MaxFelis <m.felis@flooris.nl>
2024-04-19 11:56:40 +0200

    86bydxrqk Veka logo toevoegen
    - added veka logo

--- components/General/InformationDetailBlock.vue
+++ components/General/InformationDetailBlock.vue
@@ -8,9 +8,9 @@
     </span>
     <div class="mt-4 md:mt-0 flex justify-between 3xl:w-2/3 gap-x-8" v-if="informationDetailBlock.showPartnersImages">
       <Schuco class="3xl:w-60"/>
+      <VekaLogo class="3xl:w-60"/>
       <Keralt class="3xl:w-60"/>
     </div>
-
   </div>
 </template>
 
@@ -18,6 +18,7 @@
 <script setup lang="ts">
 import Keralt from "~/icons/Keralt.vue";
 import Schuco from "~/icons/Schuco.vue";
+import VekaLogo from "~/icons/VekaLogo.vue";
 
 defineProps({
   informationDetailBlock: {
