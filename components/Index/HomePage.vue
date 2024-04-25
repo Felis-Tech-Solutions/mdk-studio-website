ede3232ead36faf826ba962dd6d9f423ef9704bc
MaxFelis <m.felis@flooris.nl>
2024-04-25 07:33:38 +0200

    86bygcgk5 Kozijnen naar homepage
    - added kozijnen to the hompage

--- components/Index/HomePage.vue
+++ components/Index/HomePage.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="flex flex-col lg:gap-y-32 h-full lg:mt-24">
     <div class="px-6 lg:px-24 xl:px-40 2xl:px-52 3xl:px-72 4xl:px-80 flex flex-col lg:gap-y-32 lg:mb-24">
-      <RoofConstructions
-          :roof-construction-data="pageData.roofConstructionComponent"
-          v-if="pageData.roofConstructionComponent" class="2xl:mx-24"
-          :page-links="pageLinks"
-      />
+    <RoofConstructions
+        :roof-construction-data="pageData.roofConstructionComponent[0]"
+        v-if="pageData.roofConstructionComponent" class="2xl:mx-24"
+        :page-links="roofConstructionsPageLinks"
+    />
       <Roadmap :roadmap-component-data="pageData.Roadmap" v-if="pageData.Roadmap"/>
+    <RoofConstructions
+        :roof-construction-data="pageData.roofConstructionComponent[1]"
+        v-if="pageData.roofConstructionComponent"
+        class="2xl:mx-24"
+        :page-links="windowPageLinks"
+    />
     </div>
     <ImageWithText :image-with-text-data="pageData.aboutUs" v-if="pageData.aboutUs"/>
     <div class="px-6 lg:px-24 2xl:px-80 flex flex-col lg:gap-y-32">
@@ -20,10 +26,16 @@ import ImageWithText from "~/components/General/ImageWithText.vue";
 import InformationBlocks from "~/components/General/InformationBlocks.vue";
 import Roadmap from "~/components/Index/Partials/Roadmap.vue";
 
-const  pageLinks = [
+const  roofConstructionsPageLinks = [
     '/dakkapellen',
     '/dakkapellen',
     '/dakkapellen'
+]
+
+const  windowPageLinks = [
+  '/deuren-kozijnen',
+  '/ramen-kozijnen',
+  '/schuifpuien-kozijnen'
 ]
 
 defineProps({
