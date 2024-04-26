817b2ef21027a508bc02b7e75f567f26e22696b8
MaxFelis <m.felis@flooris.nl>
2024-04-26 11:55:01 +0200

    86byabm9u Seo optimisatie
    - safari v14 optimisation

--- components/Index/Partials/Roadmap.vue
+++ components/Index/Partials/Roadmap.vue
@@ -9,7 +9,7 @@
     <div class="flex flex-col lg:flex-row justify-around lg:mt-16 mt-8">
       <StepComponent v-for="roadmapStep in roadmapComponentData.roadmapSteps" :roadmap-component="roadmapStep"/>
     </div>
-    <div class="flex justify-center">
+    <div class="flex justify-center lg:pt-20">
       <a href="/contact" class="bg-primary hover:bg-red-800 py-2 px-4 md:px-12 rounded-xl text-white text-sm md:text-base">
         Vraag naar de mogelijkheden
       </a>
